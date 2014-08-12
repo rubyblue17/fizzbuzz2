cb319d6961a7f8feda04edf0bde2ca4e981efd2a
rubyblue17 <RubyMac@Jonathans-iMac-2.local>
2014-08-11 20:11:16 -0400

    Final

--- fizzbuzz2.js
+++ fizzbuzz2.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
 
-var userChoice = prompt("Pick a number between 1 and 100");
+var userChoice = prompt("Pick a number between 1 and 1,000");
 
+    if(userChoice <=1000 && userChoice >=1 && userChoice % 1 == 0) {  
 
     for (var i = 1; i <= userChoice; i++) {
         if (i % 3 === 0 && i % 5 === 0){
@@ -16,10 +17,44 @@ var userChoice = prompt("Pick a number between 1 and 100");
             $("#output-list").append("<li><h3> Buzz </h3></li>")
         }
 
-        else{
+        else {
             $("#output-list").append("<li><h4>"+ i +"</h4></li>")
         }
-    };
+    }
+	}
+
+  else {
+        var userChoice = prompt("Whoops! Please enter a whole number between 1 and 1,000 (no decimals)"); 
+
+
+    if(userChoice <=1000 && userChoice >=1 && userChoice % 1 == 0) {  
+
+    for (var i = 1; i <= userChoice; i++) {
+        if (i % 3 === 0 && i % 5 === 0){
+            $("#output-list").append("<li><h1> FizzBuzz! </h1></li>")
+        }
+
+        else if (i % 3 === 0){
+            $("#output-list").append("<li><h2> Fizz </h2></li>")
+        }
+
+        else if (i % 5 === 0){
+            $("#output-list").append("<li><h3> Buzz </h3></li>")
+        }
+
+        else {
+            $("#output-list").append("<li><h4>"+ i +"</h4></li>")
+        }
+    }
+	}
+
+  else {
+        alert("Try again. Please refresh your browser and enter a whole number between 1 and 1,000 (no decimals)"); 
+        
+      };
+
+
+      };
+
 
 });
-
