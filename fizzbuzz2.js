630036c012a25e8501ed5961290c969e019f860f
rubyblue17 <RubyMac@Jonathans-iMac-2.local>
2014-08-18 18:27:23 -0400

    DRY Logic Shortened

--- fizzbuzz2.js
+++ fizzbuzz2.js
@@ -24,38 +24,8 @@ var userChoice = prompt("Pick a number between 1 and 1,000");
 	}
 
   else {
-        var userChoice = prompt("Whoops! Please enter a whole number between 1 and 1,000 (no decimals)"); 
-
-
-    if(userChoice <=1000 && userChoice >=1 && userChoice % 1 == 0) {  
-
-    for (var i = 1; i <= userChoice; i++) {
-        if (i % 3 === 0 && i % 5 === 0){
-            $("#output-list").append("<li><h1> FizzBuzz! </h1></li>")
-        }
-
-        else if (i % 3 === 0){
-            $("#output-list").append("<li><h2> Fizz </h2></li>")
-        }
-
-        else if (i % 5 === 0){
-            $("#output-list").append("<li><h3> Buzz </h3></li>")
-        }
-
-        else {
-            $("#output-list").append("<li><h4>"+ i +"</h4></li>")
-        }
-    }
-	}
-
-  else {
-        alert("Try again!"); 
+        var userChoice = alert("Whoops! Please enter a whole number between 1 and 1,000 (no decimals and no letters)"); 
         location.reload();
-        
-      };
-
 
       };
-
-
 });
